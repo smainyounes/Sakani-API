6928a6eea8117d09130a4552cb35f4e2693272ff
James <younes.abd.smain@gmail.com>
2020-07-25 10:19:46 +0100

    adding the image model with some methods

---
+++ backend/model/image.class.php
@@ -0,0 +1,111 @@
+<?php 
+
+	/**
+	 * 
+	 */
+	class model_image extends lib_database
+	{
+		
+		function __construct()
+		{
+			parent::__construct();
+		}
+
+		/**
+		 * Getters
+		 */
+
+		public function GetImages($id_local)
+		{
+			$this->query("SELECT * FROM image WHERE id_local = :id");
+			$this->bind(":id", $id_local);
+
+			return $this->resultSet();
+		}
+
+		public function GetImgLink($id_img)
+		{
+			$this->query("SELECT link FROM image WHERE id_img = :id");
+			$this->bind(":id", $id_img);
+
+			return $this->single();
+		}
+		
+		/**
+		 * Setters
+		 */
+
+		public function AddImg($id_local, $img_name)
+		{
+			$this->query("INSERT INTO image(id_local, link) VALUES(:id, :link)");
+			$this->bind(":id", $id_local);
+			$this->bind(":link", $img_name);
+
+			try {
+				$this->execute();
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+
+		public function DeleteImg($id_img)
+		{
+			$this->query("DELETE FROM image WHERE id_img = :id");
+			$this->bind(":id", $id_img);
+
+			try {
+				$this->execute();
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+
+		public function DeleteAllImgLocal($id_local)
+		{
+			$this->query("DELETE FROM image WHERE id_local = :id");
+			$this->bind(":id", $id_local);
+
+			try {
+				$this->execute();
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+
+		public function SelectMain($id_local, $id_img)
+		{
+			$this->ResetMain($id_local);
+
+			$this->query("UPDATE image SET main = 1 WHERE id_image = :id");
+
+			$this->bind(":id", $id_img);
+
+			try {
+				$this->execute();
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+
+		public function ResetMain($id_local)
+		{
+			$this->query("UPDATE image SET main = 0 WHERE id_local = :id");
+
+			$this->bind(":id", $id_local);
+
+			try {
+				$this->execute();
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+
+	}
+
+
+ ?>
