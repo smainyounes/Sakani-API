8f7a93795bf8673cf87a98ac4bd7962343d0230f
James <younes.abd.smain@gmail.com>
2020-07-25 10:19:12 +0100

    adding the local model and some methods

---
+++ backend/model/local.class.php
@@ -0,0 +1,424 @@
+<?php 
+
+	/**
+	 * 
+	 */
+	class model_local extends lib_database
+	{
+		
+		function __construct()
+		{
+			parent::__construct();
+		}
+
+		/**
+		 * Getters
+		 */
+
+		public function GetAll($page)
+		{
+			$limit = 20;
+			$start = ($page - 1) * $limit;
+
+			$sql = "SELECT *, local.id_local AS id_local
+					FROM ((local
+					INNER JOIN agence ON local.id_agence = agence.id_agence)
+					LEFT JOIN image ON local.id_local = image.id_image AND image.main = 1) 
+					WHERE agence.etat = :etat AND local.etat = :etat2 ORDER BY local.id_local DESC $limit OFFSET $start";
+
+			$this->query($sql);
+			$this->bind(":etat", "active");
+			$this->bind(":etat2", "active");
+			
+			return $this->resultSet();
+		}
+
+		public function Detail($id_local)
+		{
+			$this->query("SELECT * FROM local WHERE id_local = :id AND etat = :etat");
+
+			$this->bind(":id", $id_local);
+			$this->bind(":etat", "active");
+
+			return $this->single();
+		}
+
+		public function ByAgence($id_agence, $page)
+		{
+			$limit = 20;
+			$start = ($page - 1) * $limit;
+
+			$conc = "";
+			$etat = "";
+
+			if (isset($_SESSION['agence'])) {
+				$conc = "AND local.etat != :etat";
+				$etat = "deleted";
+			}else{
+				$conc = "AND agence.etat = :etat AND local.etat = :etat";
+				$etat = "active";
+			}
+
+			$sql = "SELECT *, local.id_local AS id_local
+							FROM ((local
+							INNER JOIN agence ON local.id_agence = agence.id_agence)
+							LEFT JOIN image ON local.id_local = image.id_image AND image.main = 1) 
+							WHERE agence.id_agence = :id $conc ORDER BY local.id_local DESC $limit OFFSET $start";
+
+			$this->query($sql);
+
+			$this->bind(":id", $id_agence);
+			$this->bind(":etat", $etat);
+
+			return $this->resultSet();
+		}
+
+		public function Search($page, $wilaya, $commune, $type, $vl)
+		{
+			$limit = 20;
+			$start = ($page - 1) * $limit;
+
+			$conc = "";
+
+			if ($wilaya !== "tout") {
+				$conc .= " AND local.wilaya = :wilaya";
+			}
+
+			if ($commune !== "tout") {
+				$conc .= " AND local.commune = :commune";
+			}
+
+			if ($categ !== "tout") {
+				$conc .= " AND local.type = :type";
+			}
+
+			if ($vl !== "tout") {
+				$conc .= " AND local.vl = :vl";
+			}
+
+			$sql = "SELECT *, local.id_local AS id_local
+					FROM ((local
+					INNER JOIN agence ON local.id_agence = agence.id_agence)
+					LEFT JOIN image ON local.id_local = image.id_image AND image.main = 1) 
+					WHERE agence.etat = :etat AND local.etat = :etat2 $conc ORDER BY local.id_local DESC $limit OFFSET $start";
+
+			$this->query($sql);
+
+			$this->bind(":etat", "active");
+			$this->bind(":etat2", "active");
+
+
+			if ($wilaya !== "tout") {
+				$this->bind(":wilaya", $wilaya);
+			}
+
+			if ($commune !== "tout") {
+				$this->bind(":commune", $commune);
+			}
+
+			if ($type !== "tout") {
+				$this->bind(":type", $type);
+			}
+
+			if ($vl !== "tout") {
+				$$this->bind(":vl", $vl);
+			}
+
+			return $this->resultSet();
+		}
+
+		public function SearchCount($wilaya, $commune, $type, $vl)
+		{
+			$conc = "";
+
+			if ($wilaya !== "tout") {
+				$conc .= " AND local.wilaya = :wilaya";
+			}
+
+			if ($commune !== "tout") {
+				$conc .= " AND local.commune = :commune";
+			}
+
+			if ($categ !== "tout") {
+				$conc .= " AND local.type = :type";
+			}
+
+			if ($vl !== "tout") {
+				$conc .= " AND local.vl = :vl";
+			}
+
+			$sql = "SELECT COUNT(id_local) nbr FROM local INNER JOIN agence ON agence.id_agence = local.id_agence WHERE agence.etat = :etat AND local.etat = :etat2 $conc";
+
+			$this->query($sql);
+
+			$this->bind(":etat", "active");
+			$this->bind(":etat2", "active");
+
+
+			if ($wilaya !== "tout") {
+				$this->bind(":wilaya", $wilaya);
+			}
+
+			if ($commune !== "tout") {
+				$this->bind(":commune", $commune);
+			}
+
+			if ($type !== "tout") {
+				$this->bind(":type", $type);
+			}
+
+			if ($vl !== "tout") {
+				$$this->bind(":vl", $vl);
+			}
+
+			$res = $this->single();
+			return $res->nbr;
+		}
+
+		public function Suggest($wilaya, $commune)
+		{
+			# code...
+		}
+
+		public function Latest($limit)
+		{
+			$sql = "SELECT *, local.id_local AS id_local
+					FROM ((local
+					INNER JOIN agence ON local.id_agence = agence.id_agence)
+					LEFT JOIN image ON local.id_local = image.id_image AND image.main = 1) 
+					WHERE agence.etat = :etat AND local.etat = :etat2 ORDER BY local.id_local DESC LIMIT :lim";
+
+			$this->query($sql);
+
+			$this->bind(":etat", "active");
+			$this->bind(":etat2", "active");
+			$this->bind(":lim", $limit);
+
+			return $this->resultSet();
+		}
+
+		/**
+		 * Setters
+		 */
+
+		public function AjouterInfos()
+		{
+			switch (strtolower($_POST['type'])) {
+				case 'appartement':
+					$this->query("INSERT INTO local(id_agence, wilaya, commune, type, vl, surface, nbr_chambre, etage, nbr_bain, prix, vu, etat) VALUES(:id, :wil, :com, :ty, :vl, :sur, :nc, :et, :bain, :pri, 0, :etat)");
+					$this->bind(":id", $_SESSION['user']);
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":nc", strip_tags(trim($_POST['nbr_chambre'])));
+					$this->bind(":et", strip_tags(trim($_POST['etage'])));
+					$this->bind(":bain", strip_tags(trim($_POST['nbr_bain'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+					$this->bind(":etat", "desactive");
+					break;
+				
+				case 'villa':
+					$this->query("INSERT INTO local(id_agence, wilaya, commune, type, vl, surface, nbr_chambre, etage, nbr_bain, piscine, nbr_garage, jardin, prix, vu, etat) VALUES(:id, :wil, :com, :ty, :vl, :sur, :nc, :et, :bain, :pisc, :gar, :jard :pri, 0, :etat)");
+					$this->bind(":id", $_SESSION['user']);
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":nc", strip_tags(trim($_POST['nbr_chambre'])));
+					$this->bind(":et", strip_tags(trim($_POST['etage'])));
+					$this->bind(":bain", strip_tags(trim($_POST['nbr_bain'])));
+					$this->bind(":pisc", strip_tags(trim($_POST['piscine'])));
+					$this->bind(":gar", strip_tags(trim($_POST['garage'])));
+					$this->bind(":jard", strip_tags(trim($_POST['jardin'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+					$this->bind(":etat", "desactive");
+					break;
+				
+				case 'arab':
+					$this->query("INSERT INTO local(id_agence, wilaya, commune, type, vl, surface, nbr_chambre, nbr_bain, jardin, nbr_garage, prix, vu, etat) VALUES(:id, :wil, :com, :ty, :vl, :sur, :nc, :bain, :jard, :gar, :pri, 0, :etat)");
+					$this->bind(":id", $_SESSION['user']);
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":nc", strip_tags(trim($_POST['nbr_chambre'])));
+					$this->bind(":bain", strip_tags(trim($_POST['nbr_bain'])));
+					$this->bind(":jard", strip_tags(trim($_POST['jardin'])));
+					$this->bind(":gar", strip_tags(trim($_POST['garage'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+					$this->bind(":etat", "desactive");
+					break;
+				
+				case 'studio':
+					$this->query("INSERT INTO local(id_agence, wilaya, commune, type, vl, surface, nbr_bain, prix, vu, etat) VALUES(:id, :wil, :com, :ty, :vl, :sur, :bain, :pri, 0, :etat)");
+					$this->bind(":id", $_SESSION['user']);
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":bain", strip_tags(trim($_POST['nbr_bain'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+					$this->bind(":etat", "desactive");
+					break;
+				
+				case 'terrain':
+					$this->query("INSERT INTO local(id_agence, wilaya, commune, type, vl, surface, prix, vu, etat) VALUES(:id, :wil, :com, :ty, :vl, :sur, :pri, 0, :etat)");
+					$this->bind(":id", $_SESSION['user']);
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+					$this->bind(":etat", "desactive");
+					break;
+				
+				default:
+					return false;
+					break;
+			}
+
+			try {
+				$this->execute();
+				return $this->LastId();
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+
+		public function EditInfos($id_local)
+		{
+			switch (strtolower($_POST['type'])) {
+				case 'appartement':
+					$this->query("UPDATE local SET wilaya = :wil, commune = :com, type = :ty, vl = :vl, surface = :sur, nbr_chambre = :nc, etage = :et, nbr_bain = :bain, prix = :pri WHERE id_local = :id");
+
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":nc", strip_tags(trim($_POST['nbr_chambre'])));
+					$this->bind(":et", strip_tags(trim($_POST['etage'])));
+					$this->bind(":bain", strip_tags(trim($_POST['nbr_bain'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+
+					$this->bind(":id", $id_local);
+					break;
+				
+				case 'villa':
+					$this->query("UPDATE local SET wilaya = :wil, commune = :com, type = :ty, vl = :vl, surface = :sur, nbr_chambre = :nc, etage = :et, nbr_bain = :bain, piscine = :pisc, nbr_garage = :gar, jardin = :jard, prix = :pri WHERE id_local = :id");
+
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":nc", strip_tags(trim($_POST['nbr_chambre'])));
+					$this->bind(":et", strip_tags(trim($_POST['etage'])));
+					$this->bind(":bain", strip_tags(trim($_POST['nbr_bain'])));
+					$this->bind(":pisc", strip_tags(trim($_POST['piscine'])));
+					$this->bind(":gar", strip_tags(trim($_POST['garage'])));
+					$this->bind(":jard", strip_tags(trim($_POST['jardin'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+
+					$this->bind(":id", $id_local);
+					break;
+				
+				case 'arab':
+					$this->query("UPDATE local SET wilaya = :wil, commune = :com, type = :ty, vl = :vl, surface = :sur, nbr_chambre = :nc, nbr_bain = :bain, jardin = :jard, nbr_garage = :gar, prix = :pri WHERE id_local = :id");
+
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":nc", strip_tags(trim($_POST['nbr_chambre'])));
+					$this->bind(":bain", strip_tags(trim($_POST['nbr_bain'])));
+					$this->bind(":jard", strip_tags(trim($_POST['jardin'])));
+					$this->bind(":gar", strip_tags(trim($_POST['garage'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+
+					$this->bind(":id", $id_local);
+					break;
+				
+				case 'studio':
+					$this->query("UPDATE local SET wilaya = :wil, commune = :com, type = :ty, vl = :vl, surface = :sur, nbr_bain = :bain, prix = :pri WHERE id_local = :id");
+
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":bain", strip_tags(trim($_POST['nbr_bain'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+
+					$this->bind(":id", $id_local);
+					break;
+				
+				case 'terrain':
+					$this->query("UPDATE local SET wilaya = :wil, commune = :com, type = :ty, vl = :vl, surface = :sur, prix = :pri WHERE id_local = :id");
+
+					$this->bind(":wil", strip_tags(trim($_POST['wilaya'])));
+					$this->bind(":com", strip_tags(trim($_POST['commune'])));
+					$this->bind(":ty", strip_tags(trim($_POST['type'])));
+					$this->bind(":vl", strip_tags(trim($_POST['vl'])));
+					$this->bind(":sur", strip_tags(trim($_POST['surface'])));
+					$this->bind(":pri", strip_tags(trim($_POST['prix'])));
+
+					$this->bind(":id", $id_local);
+					break;
+				
+				default:
+					return false;
+					break;
+			}
+
+			try {
+				$this->execute();
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+		
+		public function ChangeStat($id_local, $stat)
+		{
+			if ($stat !== "active" || $stat !== "desactive") {
+				return false;
+			}
+
+			$this->query("UPDATE local SET etat = :stat WHERE id_local = :id");
+
+			$this->bind(":stat", $stat);
+			$this->bind(":id", $id_local);
+
+			try {
+				$this->execute();
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+
+		public function Delete($id_local)
+		{
+			$this->query("UPDATE local SET etat = 'deleted' WHERE id_local = :id_local");
+
+			$this->bind(":id", $id_local);
+
+			try {
+				$this->execute();
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+
+	}
+
+ ?>
