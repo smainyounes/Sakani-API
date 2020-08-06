1b291341bbb910d3d20a256b02b53c338d624448
James <younes.abd.smain@gmail.com>
2020-08-06 15:37:17 +0100

    adding some methodes in the controller local adding the edit infos get images select main img delete img chage stat delete local

--- backend/controller/local.class.php
+++ backend/controller/local.class.php
@@ -83,6 +83,21 @@
 			}
 		}
 		
+		public function Editinfos($id_local)
+		{
+			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+				$this->forbidden($_POST['id_agence'], $_POST['tokken'], $id_local);
+
+				$mod = new model_local();
+
+				if ($mod->EditInfos($id_local)) {
+					echo json_encode(['status' => 'success']);
+				}else{
+					echo json_encode(['status' => 'error', 'data' => ['msg' => 'infos were not edited']]);
+				}
+			}
+		}
+
 		public function Addimg($id_local, $id_agence, $tokken)
 		{
 			
@@ -105,6 +120,89 @@
 				}else{
 					echo json_encode($res);
 				}
+			}
+		}
+
+		public function GetImages($id_local)
+		{
+			$v = new view_local();
+			$v->Images($id_local);
+		}
+
+		public function Selectmainimg($id_local)
+		{
+			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+				$this->forbidden($_POST['id_agence'], $_POST['tokken'], $id_local);
+
+				$mod = new model_image();
+
+				if ($mod->SelectMain($id_local, $_POST['id_img'])) {
+					echo json_encode(['status' => 'success']);
+				}else{
+					echo json_encode(['status' => 'error', 'data' => ['msg' => 'main img could not be changed']]);
+				}
+			}
+		}
+
+		public function Deleteimg($id_local, $id_img)
+		{
+			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+				$this->forbidden($_POST['id_agence'], $_POST['tokken'], $id_local);
+
+				$mod = new model_image();
+
+				if (DeletePic("img/" . $mod->GetImgLink($id_img))) {
+					if ($mod->DeleteImg($id_img)) {
+						echo json_encode(['status' => 'success']);
+					}else{
+						// not deleted from db
+						echo json_encode(['status' => 'error', 'data' => ['msg' => 'not deleted from db']]);
+					}
+				}else{
+					// img file could not be deleted
+					echo json_encode(['status' => 'error', 'data' => ['msg' => 'file not deleted']]);
+				}			
+			}
+		}
+
+		public function Changestat($id_local)
+		{
+			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+				$this->forbidden($_POST['id_agence'], $_POST['tokken'], $id_local);
+
+				$mod = new model_local();
+
+				if ($mod->ChangeStat($id_local, $_POST['etat'])) {
+					echo json_encode(['status' => 'success']);
+				}else{
+					echo json_encode(['status' => 'error', 'data' => ['msg' => 'state cound not be changed']]);
+				}
+			}
+		}
+
+		public function Deletelocal($id_local)
+		{
+			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+				$this->forbidden($_POST['id_agence'], $_POST['tokken'], $id_local);
+
+				$mod = new model_image();
+
+				$data = $mod->GetImages($id_local);
+
+				foreach ($data as $img) {
+					DeletePic('img/'.$img->link);
+				}
+
+				$mod->DeleteAllImgLocal($id_local);
+
+				$mod = new model_local();
+
+				if ($mod->Delete($id_local)) {
+					echo json_encode(['status' => 'success']);
+				}else{
+					echo json_encode(['status' => 'error']);
+				}
+
 			}
 		}
 
