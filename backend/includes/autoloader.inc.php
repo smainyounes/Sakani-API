9cab72add0880f4a12d8c2c7a3932804abf5b2b9
smainyounes <younes.abd.smain@gmail.com>
2020-12-25 15:05:40 +0100

    adding the json header in autoloader file

--- backend/includes/autoloader.inc.php
+++ backend/includes/autoloader.inc.php
@@ -4,6 +4,9 @@
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 	header("Access-Control-Allow-Headers: Content-Type,Authorization, X-Requested-With");
 
+	// remove this if the return isnt json
+	header('Content-Type: application/json');
+
 	// include confid
 	include 'config.inc.php';
 
